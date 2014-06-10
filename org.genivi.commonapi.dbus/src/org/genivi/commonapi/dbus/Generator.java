c7639b972765b60724201c3f99c077bd7149388b
Jacques GUILLOU <jacques.guillou@pelagicore.com>
2014-06-10 14:57:41 +0200

    Add command-line generator support

---
+++ org.genivi.commonapi.dbus/src/org/genivi/commonapi/dbus/Generator.java
@@ -0,0 +1,25 @@
+package org.genivi.commonapi.dbus;
+
+import java.util.List;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.xtext.generator.IFileSystemAccess;
+import org.franca.core.franca.FModel;
+import org.franca.deploymodel.dsl.fDeploy.FDInterface;
+import org.genivi.commonapi.cmdline.GeneratorInterface;
+import org.genivi.commonapi.dbus.generator.FrancaDBusGenerator;
+
+import com.google.inject.Inject;
+
+public class Generator implements GeneratorInterface {
+
+	@Inject
+	FrancaDBusGenerator generator;
+
+	public void generate(FModel fModel, List<FDInterface> deployedInterfaces,
+			IFileSystemAccess fileSystemAccess, IResource res) {
+		generator.doGenerateDBusComponents(fModel, deployedInterfaces,
+				fileSystemAccess, res);
+	}
+
+}
